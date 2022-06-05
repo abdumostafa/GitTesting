4062a732eb90f414dcde8e3c4d97a5c5e1724ccc
AbdElmneim Hussein <abdumostafa@outlook.com>
2022-06-05 16:27:52 +0200

    add GetEmployees API

--- GitTesting/Controllers/WeatherForecastController.cs
+++ GitTesting/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace GitTesting.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost(Name = "GetEmployees")]
+        public IEnumerable<WeatherForecast> GetEmployees()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
